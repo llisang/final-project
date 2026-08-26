02af9ee7838ac7dc4489d8ce61cb1ba6e0303c72
Leah Sang <llsang@emory.edu>
2026-08-26 17:39:49 -0400

    Table Summary and Regression

---
+++ R/final-project.R
@@ -0,0 +1,21 @@
+install.packages("here")
+install.packages("gtsummary", dependencies = TRUE)
+
+library(here)
+library(gtsummary)
+library(tidyverse)
+
+performance_by_nationality <- read_csv(here::here("performance_by_nationality.csv"))
+
+names(performance_by_nationality)
+
+tbl_summary(
+	performance_by_nationality,
+			by = year,
+				include = c(nationality, score, part))
+
+tbl_uvregression(
+	performance_by_nationality,
+			y = score,
+				include = c(nationality, year, part),
+					method = lm)
