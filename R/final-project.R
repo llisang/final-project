699f6856a0cc9e6efacf6c7745ed7d08edaa9980
Leah Sang <llsang@emory.edu>
2026-08-28 15:24:14 -0400

    Creating a scatterplot

--- R/final-project.R
+++ R/final-project.R
@@ -4,15 +4,19 @@ install.packages("gtsummary", dependencies = TRUE)
 library(here)
 library(gtsummary)
 library(tidyverse)
+library(ggplot2)
 
 performance_by_nationality <- read_csv(here::here("performance_by_nationality.csv"))
 
 names(performance_by_nationality)
+tbl_summary(performance_by_nationality)
 
 tbl_summary(
 	performance_by_nationality,
 			by = year,
-				include = c(nationality, score, part))
+				include = c(nationality, score, part)) |>
+	add_overall(last = TRUE)
+
 
 tbl_uvregression(
 	performance_by_nationality,
@@ -20,5 +24,17 @@ tbl_uvregression(
 				include = c(nationality, year, part),
 					method = lm)
 
+
 linear_model <- lm(score ~ nationality + year + part,
 									 data = performance_by_nationality)
+
+
+ggplot(performance_by_nationality, aes(x=score, y=type)) +
+	geom_point() +
+		labs(
+					y = "Reason for IELTS Exam",
+					x = "Test Score"
+		)
+
+
+
